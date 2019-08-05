3cc3056d0a2aeadefd47bd8dd5b35e3233bf18b5
Nayeem Jahan Rafi <njrafibd@gmail.com>
2019-08-05 22:27:29 +0600

    faq handleBars

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const path = require('path')
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
 
-const expressHbs = require('express-handlebars')
+const expressHbs = require("express-handlebars");
 
-app.engine('handleBars' , expressHbs())
-app.set('view engine', 'handleBars')
-app.set('views', 'views')
-const adminData = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+app.engine(
+  "handleBars",
+  expressHbs({ layoutsDir: "views/layouts/", defaultLayout: "main-layout" })
+);
+app.set("view engine", "handleBars");
+app.set("views", "views");
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(adminData.routes)
-app.use(shopRoutes)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(adminData.routes);
+app.use(shopRoutes);
 
-app.use((req,res,next) => {
-    // 404 error page
-    console.log("In the 404 Error Page")
-    res.status(404).render('404', { docTitle: 'Are You Lost?' , layout: false})
-    //res.status(404).sendFile(path.join(__dirname,  'views' , '404.html'))
-})
+app.use((req, res, next) => {
+  // 404 error page
+  console.log("In the 404 Error Page");
+  res.status(404).render("404", { docTitle: "Are You Lost?", layout: false });
+  //res.status(404).sendFile(path.join(__dirname,  'views' , '404.html'))
+});
 
-app.listen(3000)
+app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,21 @@
-const express = require('express')
-const path = require('path')
-const rootDir = require('../utils/path')
+const express = require("express");
+const path = require("path");
+const rootDir = require("../utils/path");
 
-const adminData = require('./admin')
-const router = express.Router()
+const adminData = require("./admin");
+const router = express.Router();
 
-router.get('/', (req,res,next) => {
-    const products = adminData.products
-    res.render('shop', {prods: products, docTitle: 'Shop' , path : '/shop' , layout: false , hasProducts : products.length > 0})
-})
+router.get("/", (req, res, next) => {
+  const products = adminData.products;
+  res.render("shop", {
+    prods: products,
+    docTitle: "Shop",
+    path: "/shop",
+    layout: true,
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCss: true
+  });
+});
 
-module.exports = router
+module.exports = router;
