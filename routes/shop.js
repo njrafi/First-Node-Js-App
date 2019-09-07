acf01b8c1f63d164607043b5ea32551730010e85
Nayeem Jahan Rafi <njrafibd@gmail.com>
2019-09-08 03:23:47 +0600

    Added Route Protection

--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const shopController = require("../controllers/shop");
+const isAuth = require("../middleware/is-auth");
 
- router.get("/", shopController.getIndex);
- router.get("/products", shopController.getProducts);
- router.get("/product/:productId", shopController.getProduct);
- router.get("/cart", shopController.getCart);
+router.get("/", shopController.getIndex);
+router.get("/products", shopController.getProducts);
+router.get("/product/:productId", shopController.getProduct);
+router.get("/cart", isAuth, shopController.getCart);
 // router.get("/checkout", shopController.getCheckout);
- router.get("/orders", shopController.getOrders);
+router.get("/orders", isAuth, shopController.getOrders);
 
- router.post("/cart", shopController.postCart);
- router.post("/delete-cart-product", shopController.postDeleteProductFromCart);
- router.post("/create-order", shopController.postOrder);
+router.post("/cart", isAuth, shopController.postCart);
+router.post(
+	"/delete-cart-product",
+	isAuth,
+	shopController.postDeleteProductFromCart
+);
+router.post("/create-order", isAuth, shopController.postOrder);
 module.exports = router;
